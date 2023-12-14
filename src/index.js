ba1d279bc6d34eeacb563930c7b46fc2db948b37
sikka1998 <sikkander1998@gmail.com>
2023-12-15 00:43:28 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
+import './index.css';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
